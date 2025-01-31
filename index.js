2225c6a99e680b3ad66bd2028c8cdb50e8557cbc
Samuel Ezealor <sen4word@gmail.com>
2025-01-31 23:21:50 +0100

    prod

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const apiRoutes = require('./routers/index')
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per windowMs
+  keyGenerator: (req) => req.ip || req.connection.remoteAddress, // Use IP instead of `X-Forwarded-For`
   message: "Too many requests from this IP, please try again later."
 });
 
