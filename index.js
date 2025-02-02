303151cd200626b98d38d3c25978d477aae23b64
Samuel Ezealor <sen4word@gmail.com>
2025-02-02 22:24:11 +0100

    prod

--- index.js
+++ index.js
@@ -28,8 +28,14 @@ const port = process.env.PORT || 4001;
 app.use(limiter);
 app.use(express.json()); // body parser
 
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
+
 // Serve Swagger Docs at /api-docs
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+  customCss:
+    '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+  customCssUrl: CSS_URL,
+}));
 
 app.use('/api', apiRoutes);
 
